babb8edadc876f1e2df1f7066c45cc57eb051d93
Areeba  Aamir <areebaamir826@gmail.com>
2025-05-05 19:47:23 +0500

    Update Employee management system.cpp
    Initial body

--- Employee management system.cpp
+++ Employee management system.cpp
@@ -1,5 +1,34 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-	
+	int portalChoice;
+	do{
+	printf("--------------------EMPLOYEE MANAGEMENT SYSTEM-------------------------\n");
+	printf("1)Admin Menu\n");
+	printf("2)Employee Menu\n");
+	printf("3)Payroll System\n");
+	printf("4)Exit\n");
+	printf("\nEnter your choice (1-4): ");
+	scanf("%d",&portalChoice);
+	if(portalChoice==1){
+		printf("---Welcome to Admin's Portal-----\n");
+		printf("Enter your password:"); 
+		
+		
+	}
+	else if(portalChoice==2){
+		printf("---Welcome to Employee's Portal-----\n");
+		printf("Enter your password:");
+		
+	}
+	else if(portalChoice==3){
+		printf("-------PayRoll System-------\n");
+		
+	}
+	else if(portalChoice==4){
+		printf("Exiting the program\n");
+	}
+} 
+while(choice!=4);
 	return 0;
 }
